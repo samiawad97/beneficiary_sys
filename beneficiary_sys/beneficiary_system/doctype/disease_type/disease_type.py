a51a5f03ed4155c9edd49194f05325ae6c927c53
samiawad97 <rakan.silent@gmail.com>
2021-07-15 18:34:42 +0300

    DocType

---
+++ beneficiary_sys/beneficiary_system/doctype/disease_type/disease_type.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, s97 and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class DiseaseType(Document):
+	pass
---
+++ beneficiary_sys/beneficiary_system/doctype/document_type/test_document_type.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, s97 and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestDocumentType(unittest.TestCase):
+	pass
